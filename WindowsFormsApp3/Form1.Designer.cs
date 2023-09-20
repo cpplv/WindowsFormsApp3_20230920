449feda01f03032da232de38eb5ec4edd6cb5d17
cpplv <89207657+cpplv@users.noreply.github.com>
2023-09-20 12:07:41 +0300

    Update Form1.Designer.cs
    poga2

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -51,7 +51,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Poga2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
